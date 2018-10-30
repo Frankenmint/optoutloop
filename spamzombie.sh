ea9f8d6e571dd876ef317f29cc4d0c21e13c60d3
Frankenmint <admin@honeybadgerofmoney.com>
2018-10-30 12:45:48 -0700

    initial commit

---
+++ spamzombie.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+COUNTER=0
+
+while :
+
+do
+
+# increment the loop
+COUNTER=$[COUNTER + 1]
+curl http://tools.braaemar.com/TNNY5yrOcgn0W.php?luahzsgaruhzhlsrscmlm/79997hk99emzzyhsbmojumslpodcxnvkti88/cghcrar/85109171337981897495899699yebl0v0lkdjsq/hkqfagx
+
+echo "we sent em sum " + COUNTER + " times"
+sleep 2
+
+done
