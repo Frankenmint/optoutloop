03575eb2bb6ad9b63d2f6e95dd5ac8835b3d9e64
Frankenmint <admin@honeybadgerofmoney.com>
2018-10-31 04:21:58 -0700

    fixed counter

--- spamzombie.sh
+++ spamzombie.sh
@@ -10,7 +10,7 @@ do
 COUNTER=$[COUNTER + 1]
 curl http://tools.braaemar.com/TNNY5yrOcgn0W.php?luahzsgaruhzhlsrscmlm/79997hk99emzzyhsbmojumslpodcxnvkti88/cghcrar/85109171337981897495899699yebl0v0lkdjsq/hkqfagx
 
-echo "we sent em sum " + COUNTER + " times"
+echo "we sent em sum "$COUNTER" times"
 sleep 2
 
 done
